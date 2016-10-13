8900c0213b01f35ffe6892f6a7653a2b275c4ad7
teojm37 <teojm37@hotmail.com>
2016-10-13 23:28:16 +0800

    Changed App Title

--- src/main/java/seedu/flexitrack/commons/core/Config.java
+++ src/main/java/seedu/flexitrack/commons/core/Config.java
@@ -11,7 +11,7 @@ public class Config {
     public static final String DEFAULT_CONFIG_FILE = "config.json";
 
     // Config values customizable through config file
-    private String appTitle = "FlexiTracker";
+    private String appTitle = "FlexiTrack";
     private Level logLevel = Level.INFO;
     private String userPrefsFilePath = "preferences.json";
     private String flexiTrackFilePath = "data/tasktracker.xml";

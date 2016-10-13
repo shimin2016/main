bdff820e4478df5c8e471b0b1d73ef45172ac6d1
Antonia Devina <antonia.devina78@gmail.com>
2016-10-13 19:03:16 +0800

    is clean XmlSerializableFlexiTrack

--- src/main/java/seedu/flexitrack/storage/XmlSerializableFlexiTrack.java
+++ src/main/java/seedu/flexitrack/storage/XmlSerializableFlexiTrack.java
@@ -21,12 +21,12 @@ import java.util.stream.Collectors;
 public class XmlSerializableFlexiTrack implements ReadOnlyFlexiTrack {
 
     @XmlElement
-    private List<XmlAdaptedTask> persons;
+    private List<XmlAdaptedTask> tasks;
     @XmlElement
     private List<Tag> tags;
 
     {
-        persons = new ArrayList<>();
+        tasks = new ArrayList<>();
         tags = new ArrayList<>();
     }
 
@@ -39,7 +39,7 @@ public class XmlSerializableFlexiTrack implements ReadOnlyFlexiTrack {
      * Conversion
      */
     public XmlSerializableFlexiTrack(ReadOnlyFlexiTrack src) {
-        persons.addAll(src.getTaskList().stream().map(XmlAdaptedTask::new).collect(Collectors.toList()));
+        tasks.addAll(src.getTaskList().stream().map(XmlAdaptedTask::new).collect(Collectors.toList()));
         tags = src.getTagList();
     }
 
@@ -57,7 +57,7 @@ public class XmlSerializableFlexiTrack implements ReadOnlyFlexiTrack {
     @Override
     public UniqueTaskList getUniqueTaskList() {
         UniqueTaskList lists = new UniqueTaskList();
-        for (XmlAdaptedTask p : persons) {
+        for (XmlAdaptedTask p : tasks) {
             try {
                 lists.add(p.toModelType());
             } catch (IllegalValueException e) {
@@ -69,7 +69,7 @@ public class XmlSerializableFlexiTrack implements ReadOnlyFlexiTrack {
 
     @Override
     public List<ReadOnlyTask> getTaskList() {
-        return persons.stream().map(p -> {
+        return tasks.stream().map(p -> {
             try {
                 return p.toModelType();
             } catch (IllegalValueException e) {

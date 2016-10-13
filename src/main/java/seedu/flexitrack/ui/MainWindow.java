8ce42030e6fd8d7e93135f28446cb93e8d957bfc
Antonia Devina <antonia.devina78@gmail.com>
2016-10-13 19:29:53 +0800

    Logic is now clean

--- src/main/java/seedu/flexitrack/ui/MainWindow.java
+++ src/main/java/seedu/flexitrack/ui/MainWindow.java
@@ -109,7 +109,7 @@ public class MainWindow extends UiPart {
 
     void fillInnerParts() {
         browserPanel = BrowserPanel.load(browserPlaceholder);
-        taskListPanel = TaskListPanel.load(primaryStage, getTaskListPlaceholder(), logic.getFilteredPersonList());
+        taskListPanel = TaskListPanel.load(primaryStage, getTaskListPlaceholder(), logic.getFilteredTaskList());
         resultDisplay = ResultDisplay.load(primaryStage, getResultDisplayPlaceholder());
         statusBarFooter = StatusBarFooter.load(primaryStage, getStatusbarPlaceholder(), config.getAddressBookFilePath());
         commandBox = CommandBox.load(primaryStage, getCommandBoxPlaceholder(), resultDisplay, logic);

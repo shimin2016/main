d4c789401d48ae5b9292108cb9d3f11280eb1061
Antonia Devina <antonia.devina78@gmail.com>
2016-10-15 22:25:01 +0800

    Duration of on event

--- src/main/java/seedu/flexitrack/model/task/DateTimeInfoParser.java
+++ src/main/java/seedu/flexitrack/model/task/DateTimeInfoParser.java
@@ -84,100 +84,91 @@ public class DateTimeInfoParser {
         }
     }
     
-    public String isFromBeforeTo (String fromTiming, String toTiming){
-        String output; 
-        if (isFromMonthBeforeToMonth(fromTiming, toTiming) == 0 ) {
-            return output = new String("This event will be happening next year!"); 
-            // Warning this is due next year 
-        } else if ( isFromMonthBeforeToMonth(fromTiming, toTiming) == 2){
-            if (isFromDateBeforeToDate(fromTiming, toTiming) == 0){
-                return output = new String("This event will be happening on the same month next year!"); 
-            } else if ( isFromDateBeforeToDate(fromTiming, toTiming) == 2 ) { 
-                if ( isFromHourBeforeToHour(fromTiming, toTiming) == 0 ){
-                    return output = new String("This event will be happening on the same date next year!"); 
-                }
-                else if ( isFromHourBeforeToHour(fromTiming, toTiming) == 2 ) { 
-                    if ( isFromMinuteBeforeToMinute(fromTiming, toTiming) == 0 ){
-                        return output = new String("This event will be happening on the same date at this hout next year!"); 
-                    }
-                    else if ( isFromMinuteBeforeToMinute(fromTiming, toTiming) == 2 ) { 
-                        return output = new String("This event will happen next year at this exact moment! Are you sure this is what you want?"
-                                +"\nTo change please use the command word 'edit'."); 
-                    }
-                }
-            }
-        }
-        return null; 
-    }
-    
     /**
      * To check if the minute inputed in 'from' is before the minute inputed in 'to'
      * @param starting Time
      * @param ending Time
      * @return 0 if it is after, 1 if it is before and 2 if they are the same 
      */
-    private int isFromMinuteBeforeToMinute(String fromTiming, String toTiming) {
-        int fromMinute = Integer.parseInt(fromTiming.substring(7,8)); 
-        int toMinute = Integer.parseInt(toTiming.substring(7,8)); 
-        if ( fromMinute > toMinute ){ 
-            return 0; 
-        } else if ( fromMinute == toMinute ){ 
-            return 2; 
+    public String durationOfTheEvent (String startingTime, String endingTime){
+        int months = monthsOfTheEvent (startingTime,endingTime);
+        int days = daysOfTheEvent (startingTime,endingTime);
+        int hours = hoursOfTheEvent (startingTime,endingTime);
+        int minutes = minutesOfTheEvent (startingTime,endingTime);
+        
+        return combineDuratingOfEvent(months,days,hours,minutes); 
+    }
+    
+    
+    private String combineDuratingOfEvent(int months, int days, int hours, int minutes) {
+        String duration = new String(""); 
+        boolean lessThanAnHour=false; 
+        boolean lessThanADay=false; 
+        boolean lessThanAMonth=false; 
+
+        if (minutes != 0) { 
+            if (minutes < 0){
+                minutes = Math.floorMod(minutes, 60); 
+                lessThanAnHour = true; 
+            }
+            duration = " " + minutes + "minute" + ((minutes == 1)?"":"s"); 
         }
-        return 1; 
+        if (hours != 0){ 
+            if (hours<0){ 
+                hours = Math.floorMod(hours, 60);
+                lessThanADay = true; 
+            }
+            if (lessThanAnHour) {
+                hours = hours - 1; 
+            }
+            duration = " " + hours + "hours" + ((hours == 1)?"":"s" + duration); 
+        }
+        if (days != 0){ 
+            if (days<0){ 
+                days = Math.floorMod(days, 60);
+                lessThanAMonth = true; 
+            }
+            if (lessThanADay) {
+                days = days - 1; 
+            }
+            duration = days + "minute" + ((days == 1)?"":"s" + duration); 
+        }
+        if (months != 0){ 
+            if (lessThanAMonth) {
+                months = months - 1; 
+            }
+            duration = months + "minute" + ((months == 1)?"":"s" + duration ); 
+        }
+        duration = "Event starts and end at the same time"; 
+        duration = duration.trim() + ".";
+        return duration;
+
     }
 
-    /**
-     * To check if the hour inputed in 'from' is before the hour inputed in 'to'
-     * @param starting Time
-     * @param ending Time
-     * @return 0 if it is after, 1 if it is before and 2 if they are the same 
-     */
-    private int isFromHourBeforeToHour(String fromTiming, String toTiming) {
-        int fromHour = Integer.parseInt(fromTiming.substring(7,8)); 
-        int toHour = Integer.parseInt(toTiming.substring(7,8)); 
-        if ( fromHour > toHour ){ 
-            return 0; 
-        }
-        else if ( fromHour == toHour ){ 
-            return 2; 
-        }
-        return 1; 
+    private static int minutesOfTheEvent(String startingTime, String endingTime) {
+        int startMinute = Integer.parseInt(startingTime.substring(7,8));
+        int endMinute = Integer.parseInt(endingTime.substring(7,8)); 
+        return endMinute - startMinute;
+    }
+    
+    private static int hoursOfTheEvent(String startingTime, String endingTime) {
+        int startHours = Integer.parseInt(startingTime.substring(7,8));
+        int endHours = Integer.parseInt(endingTime.substring(7,8)); 
+        return endHours - startHours;
+    }
+    
+    private static int daysOfTheEvent(String startingTime, String endingTime) {
+        int startDate = Integer.parseInt(startingTime.substring(4,5));
+        int endDate = Integer.parseInt(endingTime.substring(4,5)); 
+        return endDate - startDate;
     }
 
-    /**
-     * To check if the date inputed in 'from' is before the date inputed in 'to'
-     * @param starting Time
-     * @param ending Time
-     * @return 0 if it is after, 1 if it is before and 2 if they are the same 
-     */
-    private int isFromDateBeforeToDate(String fromTiming, String toTiming) {
-        int fromDate = Integer.parseInt(fromTiming.substring(4,5)); 
-        int toDate = Integer.parseInt(toTiming.substring(4,5)); 
-        if ( fromDate > toDate ){ 
-            return 0; 
-        }
-        else if ( fromDate == toDate ){ 
-            return 2; 
-        }
-        return 1;
-    }
-
-    /**
-     * To check if the month inputed in 'from' is before the month inputed in 'to'
-     * @param starting Time
-     * @param ending Time
-     * @return 0 if it is after, 1 if it is before and 2 if they are the same 
-     */
-    private int isFromMonthBeforeToMonth(String fromTiming, String toTiming) {
-        int fromMonth = DateTimeInfoParser.whatMonth(fromTiming.substring(0, 2)); 
-        int toMonth = DateTimeInfoParser.whatMonth(toTiming.substring(0, 2)); 
-        if ( fromMonth > toMonth ){ 
-            return 0;  
-        } else if (fromMonth == toMonth ) { 
-            return 2; 
-        }
-        return 1; 
+    private static int monthsOfTheEvent(String startingTime, String endingTime) {
+        String startMonth = startingTime.substring(0,2);
+        String endMonth = endingTime.substring(0, 2); 
+        int monthDifference = whatMonth(startMonth) - whatMonth(endMonth); 
+        monthDifference = Math.floorMod(monthDifference, 12);
+        return monthDifference;
     }
 
 }
